48941e761c8aeec9e4ecc527290e548908e0c695
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-09-22 10:19:57 +0300

    dom

---
+++ checkout.js
@@ -0,0 +1,39 @@
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
+
+window.addEventListener("load", () => {
+  // set items to LocalStorage
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+
+  // set items to sessionStorage
+  sessionStorage.setItem("taxRate", taxRate);
+  sessionStorage.setItem("shippingPrice", shippingPrice);
+  sessionStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
+
+const productsDiv = document.querySelector(".products");
+
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    // console.log("minus btn is clicked")
+
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (confirm("Product will be removed???")) {
+        //remove
+        event.target.parentElement.parentElement.parentElement.remove()
+      }
+    }
+  } else if (event.target.classList.contains("fa-plus")) {
+    // console.log("plus btn is clicked")
+    event.target.previousElementSibling.innerText++;
+  } else if (event.target.className == "remove-product") {
+    // console.log("remove btn is clicked")
+  } else {
+    // console.log("other clicked")
+  }
+});
